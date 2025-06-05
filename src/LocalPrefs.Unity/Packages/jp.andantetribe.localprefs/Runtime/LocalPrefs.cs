463708fa063b1ffa4af11742ec67857f9c2df412
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-06-06 02:24:09 +0900

    feat: add WebGLFileAccessorInitializer

--- src/LocalPrefs.Unity/Packages/jp.andantetribe.localprefs/Runtime/LocalPrefs.cs
+++ src/LocalPrefs.Unity/Packages/jp.andantetribe.localprefs/Runtime/LocalPrefs.cs
@@ -11,14 +11,16 @@ namespace AndanteTribe.IO.Unity
 
         static LocalPrefs()
         {
-#if ENABLE_MESSAGEPACK && UNITY_WEBGL && !UNITY_EDITOR
-            Shared = new MessagePack.MessagePackLocalPrefs("localprefs-shared", fileAccessor: new LSAccessor());
-#elif ENABLE_MESSAGEPACK
-            Shared = new MessagePack.MessagePackLocalPrefs(UnityEngine.Application.persistentDataPath + "/localprefs-shared");
-#elif UNITY_WEBGL && !UNITY_EDITOR
-            Shared = new Json.JsonLocalPrefs("localprefs-shared", fileAccessor: new LSAccessor());
+#if UNITY_WEBGL && !UNITY_EDITOR
+            const string savePath = "localprefs";
 #else
-            Shared = new Json.JsonLocalPrefs(UnityEngine.Application.persistentDataPath + "/localprefs-shared");
+            var savePath = UnityEngine.Application.persistentDataPath + "/localprefs";
+#endif
+
+#if ENABLE_MESSAGEPACK
+            Shared = new MessagePack.MessagePackLocalPrefs(savePath);
+#else
+            Shared = new Json.JsonLocalPrefs(savePath);
 #endif
         }
     }
