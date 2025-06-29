6a582e1986848eb3dfa9ad138d9c43993127709d
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-06-29 14:39:37 +0900

    feat: add debug menu base in Editor

---
+++ src/LocalPrefs.Unity/Packages/jp.andantetribe.localprefs/Editor/LocalPrefsDebugMenuWindow.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+using UnityEditor;
+using UnityEngine;
+
+namespace AndanteTribe.IO.Unity.Editor.Editor
+{
+    public class LocalPrefsDebugMenuWindow : EditorWindow
+    {
+        [MenuItem("Window/LocalPrefs/Debug Menu")]
+        public static void ShowWindow()
+        {
+            var window = GetWindow<LocalPrefsDebugMenuWindow>();
+            window.titleContent = new GUIContent("LocalPrefs Debug Menu");
+        }
+
+        public void CreateGUI()
+        {
+            var root = rootVisualElement;
+            root.AddLocalPrefsDebugMenu();
+        }
+    }
+}
