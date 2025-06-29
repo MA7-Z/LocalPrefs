efb635fee544db5bfbc7ad8df81210b26dd1148a
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-06-29 14:30:27 +0900

    feat: add debug menu base

---
+++ src/LocalPrefs.Unity/Assets/DebugToolEntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using AndanteTribe.IO;
+using AndanteTribe.IO.Unity;
+using DebugToolkit;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DebugToolEntryPoint : MonoBehaviour
+{
+    private void Start() => new DebugView().Start();
+}
+
+public sealed class DebugView : DebugViewerBase
+{
+    protected override VisualElement CreateViewGUI()
+    {
+        var root = base.CreateViewGUI();
+        var prefsDebugWindow = root.AddWindow(nameof(LocalPrefs));
+        prefsDebugWindow.style.width = Screen.width * 0.8f;
+        prefsDebugWindow.style.height = Screen.height * 0.8f;
+        prefsDebugWindow.AddLocalPrefsDebugMenu();
+        return root;
+    }
+}
---
+++ src/LocalPrefs.Unity/Packages/jp.andantetribe.localprefs/Runtime/DebugUtils.cs
@@ -0,0 +1,26 @@
+﻿#if ENABLE_UITOOLKIT
+#nullable enable
+
+using System.Runtime.CompilerServices;
+using UnityEngine.UIElements;
+
+namespace AndanteTribe.IO.Unity
+{
+    /// <summary>
+    /// Provides utility methods for debugging purposes in the LocalPrefs system.
+    /// </summary>
+    public static class DebugUtils
+    {
+        /// <summary>
+        /// Adds the LocalPrefs debug menu to the given parent element.
+        /// </summary>
+        /// <param name="parent"> The parent VisualElement to which the debug menu will be added.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AddLocalPrefsDebugMenu(this VisualElement parent)
+        {
+            parent.Add(new Button() { text = nameof(LocalPrefs) });
+        }
+    }
+}
+
+#endif
