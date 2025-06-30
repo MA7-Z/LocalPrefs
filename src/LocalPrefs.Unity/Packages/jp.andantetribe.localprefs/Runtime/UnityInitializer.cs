e50310facee51059a8091fc56cf8d57cf07cfeee
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-06-30 09:58:03 +0900

    fix: fix FileAccessor to IFileAccessor

--- src/LocalPrefs.Unity/Packages/jp.andantetribe.localprefs/Runtime/UnityInitializer.cs
+++ src/LocalPrefs.Unity/Packages/jp.andantetribe.localprefs/Runtime/UnityInitializer.cs
@@ -18,7 +18,7 @@ namespace AndanteTribe.IO.Unity
 #if UNITY_WEBGL && !UNITY_EDITOR
             var fileAccessor = new LSAccessor("localprefs-shared");
 #else
-            var fileAccessor = FileAccessor.Create(Application.persistentDataPath + "/localprefs-shared");
+            var fileAccessor = IFileAccessor.Create(Application.persistentDataPath + "/localprefs-shared");
 #endif
 
 #if ENABLE_MESSAGEPACK
